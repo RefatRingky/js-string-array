05a55f1ff3f2c652e1d5b368b37fd87f3d123204
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-14 01:44:06 +0600

    startswith commit

--- string-search.js
+++ string-search.js
@@ -1,15 +1,33 @@
+// const products =[
+//     'samsung phone',
+//     'LG supernova laptop',
+//     'HTC low price Phone',
+//     'dell hardcore laptop',
+//     'yellow Laptop with black camera',
+// ];
+// const searching = 'laptop';
+// // for  indexOf
+// const output =[];
+// for( const product of products){
+//     if (product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
+//         output.push(product);
+//     }
+// }
+// console.log(output);
+
 const products =[
+    'samsung phone',
     'samsung phone',
     'LG supernova laptop',
     'HTC low price Phone',
     'dell hardcore laptop',
     'yellow Laptop with black camera',
 ];
-const searching = 'laptop';
+const searching = 'samsung';
 // for  indexOf
 const output =[];
 for( const product of products){
-    if (product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
+    if (product.toLowerCase().startsWith(searching.toLowerCase())) {
         output.push(product);
     }
 }
