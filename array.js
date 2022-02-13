bddc8f6a49f1e87992beafe7cefaf44b28f03abf
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-14 02:37:58 +0600

    is array commit

--- array.js
+++ array.js
@@ -1,4 +1,21 @@
+// function megaFriends(friends) {
+//    let mega = friends[0];
+//    for(const friend of  friends) {
+//        if (friend.length > mega.length) {
+//            mega = friend;
+//        }
+//    }
+//    return mega;
+// }
+// const friends = ['sourav','shopnil','sourav hasan'];
+// const bigBuddy = megaFriends(friends);
+// console.log(bigBuddy);
+
+// is Array checking
 function megaFriends(friends) {
+    if (Array.isArray(friends) == false ) {
+        return 'please provide a array'
+    }
    let mega = friends[0];
    for(const friend of  friends) {
        if (friend.length > mega.length) {
@@ -8,6 +25,6 @@ function megaFriends(friends) {
    return mega;
 }
 const friends = ['sourav','shopnil','sourav hasan'];
-const bigBuddy = megaFriends(friends);
+const bigBuddy = megaFriends(12345);
 console.log(bigBuddy);
 
