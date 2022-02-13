f262363370003093e29d17898c4f693eb3bea188
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-14 03:10:49 +0600

    reverse method of aaray

---
+++ array-sort.js
@@ -0,0 +1,11 @@
+const numbers = [1,2,3,7,56,78,43,34];
+const sortedNumber = numbers.sort();
+// console.log(sortedNumber);
+const friends = ['Ringky','sourav','Tamanna'];
+const sortedFriend = friends.sort();
+console.log(sortedFriend);
+
+// reverse array
+const reverseFriends = ['Ringky','sourav','Tamanna'];
+const sortedReverseFriend = reverseFriends.reverse();
+console.log(sortedReverseFriend);
