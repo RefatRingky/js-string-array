822342e5dd180108c26720338466734e782f4255
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-14 02:20:08 +0600

    concate commit

--- string-slice.js
+++ string-slice.js
@@ -6,4 +6,10 @@
 // for slice
 const smallSlice = 'amar sonar bangla ami tomai valobasi';
 const words = smallSlice.slice(5, 13);
-console.log(words);
+// console.log(words);
+
+// concate
+const first = 'my name is';
+const second = 'Ringky';
+const fullString = first.concat(second);
+console.log(fullString);
