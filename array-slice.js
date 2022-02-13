22203de505c3eaa86e9cb70a1e07c05620cb01b4
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-14 02:55:02 +0600

    splice element

---
+++ array-slice.js
@@ -0,0 +1,6 @@
+const numbers =[1,2,45,67,6,89,65,88,90,100];
+const numberSlice = numbers.slice(4,8);
+// console.log(numberSlice);
+
+const numberSplice  = numbers.splice(4,2);
+console.log(numberSplice);
