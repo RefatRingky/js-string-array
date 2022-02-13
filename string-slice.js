860e91157c251e5451d489e625a807045e96d391
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-14 02:04:25 +0600

    slice commit

--- string-slice.js
+++ string-slice.js
@@ -1,3 +1,9 @@
-const anthem = 'amar sonar bangla ami tomai valobasi';
-const words = anthem.split(' ');
+// for split
+// const anthem = 'amar sonar bangla ami tomai valobasi';
+// const words = anthem.split(' ');
+// console.log(words);
+
+// for slice
+const smallSlice = 'amar sonar bangla ami tomai valobasi';
+const words = smallSlice.slice(5, 13);
 console.log(words);
